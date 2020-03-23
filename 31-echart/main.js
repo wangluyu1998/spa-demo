c9079661c54d6ee048c7bb033348c929aafd088f
wangluyu1998 <2602649277@qq.com>
2020-03-23 22:15:15 +0800

    -mechart&highlight

---
+++ 31-echart/main.js
@@ -0,0 +1,36 @@
+$(function(){
+  var myChart = echarts.init($('#main')[0]);
+
+  var xData = [],
+      yData = [];
+  
+  for(var p=0;p<=1;p+=0.1){
+    xData.push(p);
+    if(p === 0 || p===1){
+      yData.push(0);
+    }else{
+      yData.push(-1*p*Math.log2(p)-(1-p)*Math.log2(1-p));
+    }
+  }
+  var option = {
+    title: {
+      text: '二进熵函数曲线'
+    },
+    tooltip: {},
+      legend: {
+          data:['信息量']
+      },
+      xAxis: {
+          data:xData
+      },
+      yAxis: {},
+      series: [{
+          name: '信息量',
+          type: 'line',
+          smooth: true,
+          data: yData
+      }]
+  };
+  
+  myChart.setOption(option);
+});
